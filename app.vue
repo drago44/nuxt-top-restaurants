66a385ce9b8e118ea5b5c6711e0fbd2a7b1ca4e4
Alex <oleksander.kucherenko.work@gmail.com>
2023-03-13 17:48:49 +0200

    add bootstrap

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="container">
+    Hello
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css',
+        },
+      ],
+    },
+  },
+});
