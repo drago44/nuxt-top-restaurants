32828f61b478c3c1e4adcbb3fc8b0d63248d68cc
Alex <oleksander.kucherenko.work@gmail.com>
2023-03-13 18:06:44 +0200

    create pages

--- components/Nav.vue
+++ components/Nav.vue
@@ -5,10 +5,10 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/">Home</a>
+            <NuxtLink class="nav-link active" aria-current="page" href="/">Home</NuxtLink>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/restaurants">Restaurants</a>
+            <NuxtLink class="nav-link" href="/restaurants">Restaurants</NuxtLink>
           </li>
         </ul>
       </div>
